4290829740542972c7305aad25b23399e40fab12
Lucija Ivanković <luivanko@vsite.hr>
2016-10-20 20:16:16 +0200

    UsporedbeRealnihBrojeva done

--- UvjetnoPridruživanje/IspišiParnost.cs
+++ UvjetnoPridruživanje/IspišiParnost.cs
@@ -9,7 +9,7 @@ namespace Vsite.CSharp
             // TODO: Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti 
             // broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
             string odgovor = "neparan";
-
+            //odgovor = broj % 2==0 ? "paran" : "neparan";
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
 
